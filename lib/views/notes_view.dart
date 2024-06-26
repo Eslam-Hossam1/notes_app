00705c19c1751684128d91038cd62ffc32509f32
Eslam-Hossam1 <eslamhossam.tech@gmail.com>
2024-06-26 08:41:27 +0200

    add font awesome icon package

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,9 +9,21 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          
+          NotesAppBar(),
         ],
       ),
+    );
+  }
+}
+class NotesAppBar extends StatelessWidget {
+  const NotesAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text("Notes",style: TextStyle(),)
+      ],
     );
   }
 }
