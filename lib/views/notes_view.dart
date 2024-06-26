2047e980150aa49d0e941164b035afb2a3ba51b5
Eslam-Hossam1 <eslamhossam.tech@gmail.com>
2024-06-26 08:32:33 +0200

    create notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+  static const String id = "HomePage";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
